497d61e21479b0c81c5d854fecb6cab4261e6cb7
honorsin <keshawncui@gmail.com>
2023-07-23 21:20:47 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
+        <div>count: {count}</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => setCount(count + 1)}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
